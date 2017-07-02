0ef9f0708979e838d33eb1789893e264439111c5
Stanislav Lakhtin <savant@savant.pro>
2017-07-02 16:25:04 +0300

    minor improvments

--- src/board_setup.c
+++ src/board_setup.c
@@ -42,7 +42,7 @@
 #include <ssd1306_i2c.h>
 
 void clock_setup(void) {
-  rcc_clock_setup_in_hse_8mhz_out_72mhz();
+  rcc_clock_setup_in_hse_12mhz_out_72mhz();
 
   rcc_periph_clock_enable(RCC_GPIOA);
   rcc_periph_clock_enable(RCC_GPIOB);
